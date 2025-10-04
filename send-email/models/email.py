e103fb2109c8dff8305ff6a58e59171d05378f5f
code-masud <code.masud@hotmail.com>
2025-10-05 00:18:33 +0600

    Update email.py

--- send-email/models/email.py
+++ send-email/models/email.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import ssl
+from email.message import EmailMessage
 
 class Email(ABC):
     def __init__(self, host, port, sender, password):
@@ -10,11 +11,11 @@ class Email(ABC):
         self.context = ssl.create_default_context()
 
     @abstractmethod
-    def create_email(self):
+    def create_email(self, receiver, subject, text_body, html_body = None, attachment_path = None) -> EmailMessage:
         pass
 
     @abstractmethod
-    def send_email(self):
+    def send_email(self, message: EmailMessage):
         pass
 
     def __str__(self):
